c37ca297227d8c1300657e4bb11e95a76b362f76
Luis Blanco <luisbastian.blanco@gmail.com>
2024-05-31 15:42:10 -0400

    update project with information

---
+++ src/utils/projects.ts
@@ -0,0 +1,44 @@
+export const projects = [
+	{
+		title: 'Real Windows',
+		body: 'Diseño y desarrollo web para empresa Real Windows',
+		href: 'https://web.realwindows.cl',
+		tech: 'NextJS, TailwindCSS, Javascript',
+		image: '/projects/realwindows.webp'
+	},
+	{
+		title: 'Stop&Go Chile',
+		body: 'Diseño y desarrollo web para empresa Stop&Go',
+		href: 'https://www.stopgochile.cl',
+		tech: 'NextJS, TailwindCSS, Javascript',
+		image: '/projects/stopgo.webp'
+	},
+    {
+		title: 'Tecmedi',
+		body: 'Diseño y desarrollo web para empresa Tecmedi',
+		href: 'https://tecmedi.cl',
+		tech: 'NextJS, TailwindCSS, Javascript',
+		image: '/projects/tecmedi.webp'
+	},
+    {
+		title: 'Farma7',
+		body: 'Diseño y desarrollo web para empresa Farma7',
+		href: 'https://www.farma7.cl',
+		tech: 'NextJS, TailwindCSS, Javascript',
+		image: '/projects/farma7.webp'
+	},
+    {
+		title: 'Ecofarmacias',
+		body: 'Diseño y desarrollo web para empresa Ecofarmacias',
+		href: 'https://www.farma7.cl',
+		tech: 'Wordpress, WooCommerce, PHP',
+		image: '/projects/ecofarmacias.webp'
+	},
+    {
+		title: 'VITOXS',
+		body: 'Diseño y desarrollo web para empresa VITOXS',
+		href: 'https://www.vitoxs.cl',
+		tech: 'Wordpress, WooCommerce, PHP',
+		image: '/projects/vitoxs.webp'
+	}
+]
